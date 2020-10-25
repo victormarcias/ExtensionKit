76efce4aee05021ab595cd847042018869f95bbf
Victor Marcias <victormarcias@gmail.com>
2020-10-25 16:08:00 -0300

    Fixed PrimeNumbers algorithm.

--- ExtensionKit/Math/Int+PrimeNumbers.swift
+++ ExtensionKit/Math/Int+PrimeNumbers.swift
@@ -8,21 +8,42 @@
 
 import Foundation
 
+///
+/// Sources:
+///     - https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+///     - https://en.wikipedia.org/wiki/Sieve_of_Sundaram
+///     - https://www.mathsisfun.com/prime_numbers.html
+/// Solution:
+///     - https://stackoverflow.com/questions/45784858/identify-prime-number
+///
 extension Int {
+    
+    /// Returns a list of prime numbers up until a given number
+    ///
+    /// - Returns: an array with the prime numbers list
+    static func primeNumbers(in range: Int) -> [Int] {
+        return primeNumbers(in: 2...range)
+    }
     
     /// Returns a list of prime numbers within a given range
     ///
     /// - Returns: an array with the prime numbers list
     static func primeNumbers(in range: ClosedRange<Int>) -> [Int] {
-        let primes = Array(range)
-            .filter { $0 > 1 }                  // filter negatives, zero, one
-            .filter {
-                ($0 % 2 != 0 || $0 == 2) &&     // filter even numbers
-                ($0 % 3 != 0 || $0 == 3) &&     // filter multiples of 3
-                ($0 % 5 != 0 || $0 == 5) &&     // filter multiples of 5
-                ($0 % 7 != 0 || $0 == 7)        // filter multiples of 7
-            }
+        let valid = Array(range).filter { $0 > 1 }
         
-        return primes
+        let result = valid.filter {
+            let wheel = [2,3,5,7,11,13,17,19]
+            
+            // light solution
+            if wheel.contains($0) {
+                return true
+            }
+            // heavy
+            let number = $0
+            let upperLimit = Int(Double(number).squareRoot())
+            let isPrime = !(2...upperLimit).contains { number % $0 == 0 }
+            return isPrime
+        }
+        return result
     }
 }
--- ExtensionKitTests/Math/Int+PrimeNumbers_Tests.swift
+++ ExtensionKitTests/Math/Int+PrimeNumbers_Tests.swift
@@ -17,12 +17,54 @@ class Int_PrimeNumbers_Tests: XCTestCase {
                    53, 59, 61, 67, 71,
                    73, 79, 83, 89, 97]
     
-    func test_first25() {
-        let sut = Int.primeNumbers(in: 1...100)
-        XCTAssert(sut.count == 25)
+    func testBasicRange() {
+        let sut = Int.primeNumbers(in: 100)
         
         for (i, _) in sut.enumerated() {
             XCTAssert(sut[i] == first25[i])
+        }
+    }
+    
+    func testPrimeNumbersWheel() {
+        let sut = Int.primeNumbers(in: 20)
+        let expected = [2,3,5,7,11,13,17,19]
+        
+        for (i, _) in sut.enumerated() {
+            XCTAssert(sut[i] == expected[i])
+        }
+    }
+    
+    func testPrimeNumbersCount() {
+        let sut10 = Int.primeNumbers(in: 10).count
+        let sut100 = Int.primeNumbers(in: 100).count
+        let sut1000 = Int.primeNumbers(in: 1_000).count
+        let sut10000 = Int.primeNumbers(in: 10_000).count
+        let sut100000 = Int.primeNumbers(in: 100_000).count
+        //let sut1000000 = Int.primeNumbers(in: 1_000_000).count
+        
+        XCTAssert(sut10 == 4)
+        XCTAssert(sut100 == 25)
+        XCTAssert(sut1000 == 168)
+        XCTAssert(sut10000 == 1229)
+        XCTAssert(sut100000 == 9592) // ~1 sec.
+        //XCTAssert(sut1000000 == 78498) // ~30 sec.
+    }
+    
+    func testRandomRange() {
+        let sut = Int.primeNumbers(in: -2500...13)
+        let expected = [2,3,5,7,11,13]
+        
+        for (i, _) in sut.enumerated() {
+            XCTAssert(sut[i] == expected[i])
+        }
+    }
+    
+    func testInclusiveRange() {
+        let sut = Int.primeNumbers(in: 11...19)
+        let expected = [11,13,17,19]
+        
+        for (i, _) in sut.enumerated() {
+            XCTAssert(sut[i] == expected[i])
         }
     }
 }
